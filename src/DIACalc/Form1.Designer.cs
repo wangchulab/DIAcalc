8113f89dfbffada1df00fc3755ef8960e3d81c88
GuogengJia <61615467+GuogengJia@users.noreply.github.com>
2021-11-19 22:27:26 +0800

    Add files via upload

---
+++ src/DIACalc/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DIACalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialogData = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogFasta = new System.Windows.Forms.OpenFileDialog();
+            this.open = new System.Windows.Forms.Button();
+            this.openfasta = new System.Windows.Forms.Button();
+            this.calc = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.target = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "rep";
+            // 
+            // openFileDialogData
+            // 
+            this.openFileDialogData.FileName = "openFileDialog1";
+            // 
+            // openFileDialogFasta
+            // 
+            this.openFileDialogFasta.FileName = "openFileDialog2";
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(82, 66);
+            this.open.Margin = new System.Windows.Forms.Padding(2);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(86, 26);
+            this.open.TabIndex = 0;
+            this.open.Text = "open file";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // openfasta
+            // 
+            this.openfasta.Location = new System.Drawing.Point(212, 66);
+            this.openfasta.Margin = new System.Windows.Forms.Padding(2);
+            this.openfasta.Name = "openfasta";
+            this.openfasta.Size = new System.Drawing.Size(92, 26);
+            this.openfasta.TabIndex = 1;
+            this.openfasta.Text = "open fasta";
+            this.openfasta.UseVisualStyleBackColor = true;
+            this.openfasta.Click += new System.EventHandler(this.openfasta_Click);
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(212, 182);
+            this.calc.Margin = new System.Windows.Forms.Padding(2);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(88, 30);
+            this.calc.TabIndex = 2;
+            this.calc.Text = "calc";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(340, 66);
+            this.save.Margin = new System.Windows.Forms.Padding(2);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(94, 26);
+            this.save.TabIndex = 3;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // target
+            // 
+            this.target.Location = new System.Drawing.Point(252, 133);
+            this.target.Margin = new System.Windows.Forms.Padding(2);
+            this.target.Name = "target";
+            this.target.Size = new System.Drawing.Size(68, 21);
+            this.target.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "target";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 240);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(424, 22);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 300);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.target);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.openfasta);
+            this.Controls.Add(this.open);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "DIAcalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogData;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFasta;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button openfasta;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ src/DIACalc/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIACalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialogData.Filter = "txt(*.txt)|*.txt";
+            openFileDialogFasta.Filter = "fasta(*.fasta)|*.fasta";
+            saveFileDialog1.Filter = "txt(*.txt)|*.txt";
+            target.Text = "IA_acid";
+            calc.Enabled = false;
+            Program.SetProgressbar += SetProEvent;
+        }
+
+        public static string datapath = "";
+        public static string fastapath = "";
+        public static string savepath = "rep.txt";
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogData.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                datapath = openFileDialogData.FileName;
+                savepath = openFileDialogData.FileName.Split('.')[0] + "_rep.txt";
+                saveFileDialog1.FileName = savepath;
+                if (datapath != "" && fastapath != "")
+                {
+                    calc.Enabled = true;
+                }
+                else
+                {
+                    calc.Enabled = false;
+                }
+            }
+        }
+
+        private void openfasta_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogFasta.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fastapath = openFileDialogFasta.FileName;
+                Program.ClearProtein();
+                if (datapath != "" && fastapath != "")
+                {
+                    calc.Enabled = true;
+                }
+                else
+                {
+                    calc.Enabled = false;
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                savepath = saveFileDialog1.FileName;
+                //MessageBox.Show(savepath);
+            }
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            if (target.Text == "")
+            {
+                MessageBox.Show("No valid target information");
+                return;
+            }
+            Thread thread = new Thread(new ThreadStart(Calc));
+            thread.IsBackground = true;
+            thread.Start();
+            EnableButton(false);
+            
+        }
+
+        private void cutoff_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private delegate void SetProgress(int i);
+
+        private delegate void Enable(bool b);
+
+        private void EnableButton(bool b)
+        {
+            this.calc.Enabled = b;
+            this.save.Enabled = b;
+            this.open.Enabled = b;
+            this.openfasta.Enabled = b;
+            this.target.Enabled = b;
+        }
+
+        void SetProEvent(object sender, int i)
+        {
+            Setpro(i);
+        }
+
+        public void Setpro(int i)
+        {
+            if (this.InvokeRequired)
+            {
+                SetProgress setprogress = new SetProgress(Setpro);
+                this.Invoke(setprogress, new object[] { i });
+            }
+            else
+            {
+                this.progressBar1.Value = i;
+            }
+        }
+
+        private void Calc()
+        {
+            Setpro(0);
+            Program.ReadFasta(fastapath);
+            Setpro(5);
+            //Thread thread = new Thread(new ThreadStart(Time));
+            //thread.IsBackground = true;
+            //thread.Start();
+            int singlecnt = 0;
+
+            string r = Program.Result(target.Text, datapath, false,int.MaxValue, out singlecnt,-1, true, 1);
+            //thread.Abort();
+            Setpro(90);
+            StreamWriter sw = new StreamWriter(savepath);
+            sw.Write(r);
+            sw.Close();
+            Setpro(100);
+            //singlecount.Text = "singlecount: " + singlecnt.ToString();
+            MessageBox.Show("Finished");
+            Enable enable = new Enable(EnableButton);
+            this.Invoke(enable, new object[] { true });
+        }
+
+        private void Time()
+        {
+            int k = 1000;
+            for (int i = 0; i < k; i++)
+            {
+                Thread.Sleep(500);
+                Setpro(i * 85 / k + 5);
+            }
+        }
+
+    }
+}
